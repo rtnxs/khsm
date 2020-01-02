34fcaee43ececb4a27d271b66961f80ed5e1e94a
rtnxs <riotnexus@mail.ru>
2020-01-02 19:38:00 +0300

    rspec Game#answer_current_question

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -82,11 +82,9 @@ RSpec.describe Game, type: :model do
     end
 
     context 'correct .status' do
-      #before(:each) do
-      #  game_w_questions.finished_at = Time.current
-      #end
-      let! { game_w_questions.finished_at = Time.current }
-
+      before(:each) do
+        game_w_questions.finished_at = Time.current
+      end
 
       it ':won' do
         game_w_questions.current_level = Question::QUESTION_LEVELS.max + 1
